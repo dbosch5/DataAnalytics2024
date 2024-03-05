7230d4435ce22fe8426a6589999745accf82dd8b
dbosch5 <138499403+dbosch5@users.noreply.github.com>
2024-03-05 13:30:03 +0100

    Add files via upload

---
+++ TascaS4.01/tascaS4.01.sql
@@ -0,0 +1,31 @@
+# Nivell 1
+
+/** Descàrrega els arxius CSV, estudia'ls i dissenya una base de dades amb un esquema d'estrella que contingui, 
+almenys 4 taules de les quals puguis realitzar les següents consultes: **/
+
+## Exercici 1
+
+/** Realitza una subconsulta que mostri tots els usuaris amb més de 30 transaccions utilitzant almenys 2 taules. **/
+
+## Exercici 2
+
+/** Mostra la mitjana de la suma de transaccions per IBAN de les targetes de crèdit en la companyia Donec Ltd. 
+utilitzant almenys 2 taules. **/
+
+# Nivell 2
+
+/** Crea una nova taula que reflecteixi l'estat de les targetes de crèdit basat en si les últimes tres transaccions 
+van ser declinades i genera la següent consulta: **/
+
+## Exercici 1
+
+/** Quantes targetes estan actives? **/
+
+# Nivell 3
+
+/** Crea una taula amb la qual puguem unir les dades del nou arxiu products.csv amb la base de dades creada, 
+tenint en compte que des de transaction tens product_ids. Genera la següent consulta: **/
+
+## Exercici 1
+
+/** Necessitem conèixer el nombre de vegades que s'ha venut cada producte. **/
